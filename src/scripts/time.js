94615a595604c29f22a52f563cbaa0db6b9f4d92
Koetemagie <koetemagie@gmail.com>
2021-01-22 02:02:48 -0300

    added more styles, fixed grid

---
+++ src/scripts/time.js
@@ -0,0 +1,24 @@
+// load time in the screen
+const startTime = () => {
+  const date = new Date();
+  const [h,m,s] = [date.getHours(), date.getMinutes(), date.getSeconds()].map(e => formatTime(e));
+  document.getElementById('time').innerHTML = `${h}:${m}:${s}`;
+  setTimeout('startTime()', 500);
+}
+
+// format time for better output (with zeros)
+const formatTime = i => {
+  if (i < 10) i = "0" + i;
+  return i;
+}
+
+// date
+const loadDate = () => {
+  const date = new Date();
+  const options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' };
+  document.getElementById('nowaday').innerHTML = date.toLocaleString('en-gb', options);
+
+  startTime()
+}
+
+loadDate()
